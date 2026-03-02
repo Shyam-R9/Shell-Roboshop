a50e95caea4c8f6f163e4fe70cedd18c0becd65f
shyam <shyam9.r9@gmail.com>
2026-03-02 10:11:47 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -15,39 +15,81 @@ log () {
     echo -e "${G}$(date '+%Y-%m-%d %H:%M:%S')${N} $msg" | tee -a $LOG_FILE
 }
 
+check_status() {
+    local status=$1
+    local message=$2
+    if status -eq 0; then
+        log echo -e "${G}$2 is success${N}"
+    else
+        log echo -e "${R}$2 is failed${N}"
+        exit 1
+    fi
+}
+
 log "${G}Disabling the default version of nodejs download${N}"
 dnf module disable nodejs -y
+check_status $? "Disabling is"
 
-log "${G}enable nodejs 20 download stream${N}"
+log "${G}enabling the nodejs 20 download stream${N}"
 dnf module enable nodejs:20 -y
+check_status $? "Enbling NodeJS20 is"
 
 log "${G}install NodeJS 20${N}"
 dnf install nodejs -y
+check_status $? "Installation of NodeJs"
 
-log "${G}Create roboshop user${N}"
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop service account" roboshop
+log "${G}Checking if the roboshop user already exists${N}"
+if [ id -u roboshop ]; then
+    log "${Y}roboshop user already exists, skipping user creation${N}"
+else
+    log "${G}roboshop user doesn't exist, proceeding with user creation${N}"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop service account" roboshop
+    check_status $? "Roboshop user creation "
+fi
 
-log "${G}create application folder${N}"
-mkdir /app
+log "${G}Checking if the application folder already exists${N}"
+if [ -d /app ]; then
+    log "${Y}/app folder already exists, skipping folder creation${N}"
+else
+    log "${G}/app folder doesn't exist, proceeding with folder creation${N}"
+    mkdir /app
+    check_status $? "/app folder creation "
+fi
 
-log "${G}Download application code${N}"
+log "${G}Downloading catolouge application code to temp folder${N}"
+rm -rf /app/*
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+check_status $? "Downloading of catalogue.zip"
 
-log "${G}Install dependencies${N}"
+log "${G}Installing dependencies${N}"
 cd /app
 unzip /tmp/catalogue.zip
 npm install
+check_status $? "Installing dependencies"
 
-log "${G}create catalogue service file and copy services folder${N}"
+log "${G}Copy the catalogue.service file to the systemd unit files director${N}"
 cp catalogue.service /etc/systemd/system/
+check_status $? "Copy of catalogue.service file"
 
-log "${G}Enable and Start service${N}"
+log "${G}Enable catalogue service${N}"
 systemctl enable catalogue.service
-systemctl start catalogue.service
 
-log "${G}Create mongodb repo and install Mongo client${N}"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+log "${G}Start Catalogue service${N}"
+systemctl start catalogue.service
+while ! systemctl is-active --quiet catalogue.service; do
+    echo "Waiting for Catalogue service to be active"
+    sleep 2
+done
+log "${G}Catalogue service is now active${N}"
+
+log "${G}Copy mongo repo file${N}"
+cp mongo.repo /etc/yum.repos.d/
+check_status $? "Copy of the mongo repo file"
+
+log "${G}Install mongo client${N}"
 dnf install mongodb-mongosh -y
+check_status $? "Istallation of mongo"
 
 log "${G}Load master data${N}"
 mongosh --host MONGODB </app/db/master-data.js
+check_status $? "Loading of the master data to mongo db"
