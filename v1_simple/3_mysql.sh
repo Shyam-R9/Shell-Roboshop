2966bd2ac4042a3ae25e3cecc8b78087f70d1399
shyam <shyam9.r9@gmail.com>
2026-03-07 22:47:31 +0530

    roboshop scritps

---
+++ v1_simple/3_mysql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIRECTORY=$(cd "$(dirname "$0")" && pwd)
+
+LOG_File="/var/log/mysql-server-install.log"
+
+exec > >(awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee -a "$LOG_File")
+exec 2>&1
+
+echo "Installing mysql-server"
+dnf install mysql-server -y
+
+echo "Enabling mysql-server service"
+systemctl enable mysqld
+
+echo "Starting mysql service and waiting until it comes to active state"
+systemctl start mysqld
+while ! systemctl is-active --quiet mysqld; do
+    echo "Waiting for mysql service to be active"
+    sleep 5
+done
+echo "mysql service is now active"
+
+echo "Change the default root password"
+mysql_secure_installation --set-root-pass RoboShop@1
+
+echo "mysql service status:
+systemctl status mysqld
--- v1_simple/4_rabbitmq.sh
+++ v1_simple/4_rabbitmq.sh
@@ -3,27 +3,34 @@ set -euo pipefail
 
 SCRIPT_DIRECTORY=$(cd "$(dirname "$0")" && pwd)
 
-LOG_FILE="/var/log/rabbitmq-install.log"
+LOG_File="/var/log/rabbitmq-server-install.log"
 
-exec > >(awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0 }' | tee -a $LOG_File)
+exec > >(awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee -a "$LOG_File")
 exec 2>&1
 
 USER="roboshop"
 PASSWORD="roboshop123"
 
-echo "Starting RabbitMQ installation"
-
+echo "Copying rabbitmq repo to repos directory"
 cp "$SCRIPT_DIRECTORY/rabbitmq.repo" /etc/yum.repos.d/
 
+echo "Starting RabbitMQ installation"
 dnf install rabbitmq-server -y
 
+echo "Enable  and start rabbitmq-server service"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
+echo "Waiting until rabbitmq service comes to active state"
+while ! systemctl is-active --quiet rabbitmq-server; do
+    echo "Waiting for rabbitmq-server service to be active"
+    sleep 5
+done
+echo "rabbitmq-server service is now active"
+
+echo "Adding a user rabbitmq to access the database"
 rabbitmqctl add_user $USER $PASSWORD
-
 rabbitmqctl set_permissions -p / $USER ".*" ".*" ".*"
-
 rabbitmqctl status
 
 echo "RabbitMQ installation completed"
