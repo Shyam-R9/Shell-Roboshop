5c852b3bc8d31370b9220eb01a201ad96a8be9dd
shyam <shyam9.r9@gmail.com>
2026-03-09 17:07:04 +0530

    roboshop scripts

--- v1_simple/3_mysql.sh
+++ v1_simple/3_mysql.sh
@@ -1,55 +1,34 @@
 #!/bin/bash
+#### steps to cleanup mysql###
+#dnf remove mysql-server -y
+#rm -rf /var/lib/mysql . previous installation left data in /var/lib/mysql
+#dnf install mysql-server -y
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+MYSQL_ROOT_PASSWORD="RoboShop@1"
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+# Install MySQL
+dnf install mysql-server -y
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+# Enable & start service
+systemctl enable mysqld
+systemctl start mysqld
+
+# Wait for MySQL
+until mysqladmin ping --silent; do
+  echo "Waiting for MySQL to start..."
+  sleep 2
+done
+
+# Check if root login without password works
+if mysql -u root -e "SELECT 1" >/dev/null 2>&1; then
+    echo "Setting MySQL root password..."
+
+    mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+    echo "Root password configured."
 else
-    echo "You are running with root access" | tee -a $LOG_FILE
+    echo "Root password already set. Skipping."
 fi
-
-echo "Please enter root password to setup"
-read -s MYSQL_ROOT_PASSWORD
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "Installing MySQL server"
-
-systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabling MySQL"
-
-systemctl start mysqld   &>>$LOG_FILE
-VALIDATE $? "Starting MySQL"
-
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
-VALIDATE $? "Setting MySQL root password"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
