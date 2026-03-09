a8cfa006b1707ffbfcbe37b6e92e22cb3e0329ab
shyam <shyam9.r9@gmail.com>
2026-03-09 17:39:09 +0530

    roboshop scripts

--- v1_simple/3_mysql.sh
+++ v1_simple/3_mysql.sh
@@ -3,23 +3,34 @@
 #dnf remove mysql-server -y
 #rm -rf /var/lib/mysql . previous installation left data in /var/lib/mysql
 #dnf install mysql-server -y
+SCRIPT_DIRECTORY=$(cd "$(dirname "$0")" && pwd)
+
+LOG_File="/var/log/redis-install.log"
+
+exec > >(awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee -a "$LOG_File")
+exec 2>&1
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
 
 MYSQL_ROOT_PASSWORD="RoboShop@1"
 
-# Install MySQL
+echo "Installing MySQL"
 dnf install mysql-server -y
 
-# Enable & start service
+echo "Enable & start service"
 systemctl enable mysqld
 systemctl start mysqld
 
-# Wait for MySQL
+echo "Waiting for MySQL to start"
 until mysqladmin ping --silent; do
   echo "Waiting for MySQL to start..."
   sleep 2
 done
 
-# Check if root login without password works
+echo "Check if root login without password works"
 if mysql -u root -e "SELECT 1" >/dev/null 2>&1; then
     echo "Setting MySQL root password..."
 
